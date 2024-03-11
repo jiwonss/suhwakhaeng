c92c0042a8e0a926888d9d6b6b831ce2eb632190
오민상 <flos9537@gmail.com>
2024-03-11 22:43:57 +0900

    [FE] feat: 좋아요 버튼 구현

---
+++ frontend/src/components/button/ButtonStyle.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import { widthPercent, heightPercent } from '../../config/dimension/Dimension';
+
+export const LikeButtonStyle = styled.View`
+    width: ${widthPercent * 46}px;
+    height: ${heightPercent * 46}px;
+`;
--- frontend/src/components/button/LikeButton.tsx
+++ frontend/src/components/button/LikeButton.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import * as React from 'react';
+import LikeActive from '../../../assets/icons/likeActive.svg';
+import LikeDefault from '../../../assets/icons/likeDefault.svg';
+import { LikeButtonStyle } from './ButtonStyle';
+import { TouchableOpacity, View } from 'react-native';
 
-import { Text, View } from 'react-native';
+interface SvgComponentProps {
+  onPress: () => void;
+}
 
 const LikeButton = () => {
-  return (
-    <Text>
+  const [isActive, setIsActive] = React.useState(false);
 
-    </Text>
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  // 좋아요 활성화
+  const LikeActiveComponent: React.FC<SvgComponentProps> = ({ onPress }) => (
+    <TouchableOpacity onPress={onPress} style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <LikeActive width='46' height='46' />
+    </TouchableOpacity>
+  );
+
+  // 좋아요 비활성화
+  const LikeDefaultComponent: React.FC<SvgComponentProps> = ({ onPress }) => (
+    <TouchableOpacity onPress={onPress} style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <LikeDefault width='46' height='46' />
+    </TouchableOpacity>
+  );
+
+  return (
+    <LikeButtonStyle>
+      <View style={{ width: 46, height: 46, alignItems: 'center', justifyContent: 'center' }}>
+        {isActive ? <LikeActiveComponent onPress={toggleActive} /> : <LikeDefaultComponent onPress={toggleActive} />}
+      </View>
+    </LikeButtonStyle>
   );
 };
 
