dad35baa60075e9ff7e1a160347b68708bd70bf7
오민상 <flos9537@gmail.com>
2024-03-12 09:34:19 +0900

    [FE] feat: 프로필 수정 버튼 컴포넌트 구현

--- frontend/src/components/button/ButtonStyle.tsx
+++ frontend/src/components/button/ButtonStyle.tsx
@@ -1,24 +1,37 @@
 import styled from 'styled-components/native';
 import { heightPercent, widthPercent } from '../../config/dimension/Dimension';
 import { TouchableOpacity } from 'react-native';
-import { GREEN500 } from '../../config/color/Color';
+import * as Color from '../../config/color/Color';
 
+// 좋아요 버튼 스타일
 export const LikeButtonStyle = styled.View`
     width: ${widthPercent * 46}px;
     height: ${heightPercent * 46}px;
 `;
-
+// 매시지 보내기 버튼 스타일
 export const SubmitButtonStyle = styled(TouchableOpacity)`
     align-items: center;
     justify-content: center;
     width: ${widthPercent * 36}px;
     height: ${heightPercent * 36}px;
 `;
+
+// 장터 이동하기 버튼 스타일
 export const ShopButtonStyle = styled(TouchableOpacity)`
     align-items: center;
     justify-content: center;
     width: ${widthPercent * 100}px;
     height: ${heightPercent * 40}px;
-    background-color: ${GREEN500};
+    background-color: ${Color.GREEN500};
     border-radius: 10px;
+`;
+
+// 프로필 수정 버튼 스타일
+export const ProfileSettingButtonStyle = styled(TouchableOpacity)`
+    align-items: center;
+    justify-content: center;
+    width: ${widthPercent * 150}px;
+    height: ${heightPercent * 30}px;
+    border-radius: 10px;
+    border: 0.4px solid ${Color.GRAY400};
 `;
--- frontend/src/components/button/ProfileSettingsButton.tsx
+++ frontend/src/components/button/ProfileSettingsButton.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-
-import { Text, View } from 'react-native';
+import * as React from 'react';
+import { View } from 'react-native';
+import { ProfileSettingButtonStyle } from './ButtonStyle';
+import { BODY4_M } from '../typography/Typography';
 
 const ProfileSettingButton = () => {
-  return (
-    <Text>
+  const clickSubmit = () => {
+    console.log('click submit');
+    // API 호출 코드 작성
+  };
 
-    </Text>
+  return (
+    <View style={{ padding: 10 }}>
+      <ProfileSettingButtonStyle onPress={clickSubmit}>
+        <BODY4_M >프로필 수정</BODY4_M>
+      </ProfileSettingButtonStyle>
+    </View>
   );
 };
 
