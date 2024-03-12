17dcf37a1d58e20688f2a4a1f5456d40415636fa
오민상 <flos9537@gmail.com>
2024-03-12 09:27:48 +0900

    [FE] feat: 장터 이동하기 버튼 컴포넌트 구현

--- frontend/src/components/button/ButtonStyle.tsx
+++ frontend/src/components/button/ButtonStyle.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components/native';
 import { heightPercent, widthPercent } from '../../config/dimension/Dimension';
 import { TouchableOpacity } from 'react-native';
+import { GREEN500 } from '../../config/color/Color';
 
 export const LikeButtonStyle = styled.View`
-  width: ${widthPercent * 46}px;
-  height: ${heightPercent * 46}px;
+    width: ${widthPercent * 46}px;
+    height: ${heightPercent * 46}px;
 `;
 
 export const SubmitButtonStyle = styled(TouchableOpacity)`
@@ -12,5 +13,12 @@ export const SubmitButtonStyle = styled(TouchableOpacity)`
     justify-content: center;
     width: ${widthPercent * 36}px;
     height: ${heightPercent * 36}px;
-
+`;
+export const ShopButtonStyle = styled(TouchableOpacity)`
+    align-items: center;
+    justify-content: center;
+    width: ${widthPercent * 100}px;
+    height: ${heightPercent * 40}px;
+    background-color: ${GREEN500};
+    border-radius: 10px;
 `;
