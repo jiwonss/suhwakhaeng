dcb6a9cff3858628717dfa33ecee12a5210ee02a
서지원 <sjs3326@naver.com>
2024-03-14 14:50:02 +0900

    [INFRA] chore: common 서버에 eureka client 적용

--- backend/common/src/main/java/com/suhwakhaeng/common/CommonApplication.java
+++ backend/common/src/main/java/com/suhwakhaeng/common/CommonApplication.java
@@ -1,9 +1,15 @@
 package com.suhwakhaeng.common;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@EnableDiscoveryClient
 public class CommonApplication {
 
 	public static void main(String[] args) {
