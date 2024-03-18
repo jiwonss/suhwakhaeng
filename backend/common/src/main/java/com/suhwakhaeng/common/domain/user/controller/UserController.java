3f6e8460d87964b3d42200922e4ac5657d4f0e98
박정인 <wjddlsee@gmail.com>
2024-03-18 17:37:10 +0900

    [BE] feat: 회원정보 조회 구현

--- backend/common/src/main/java/com/suhwakhaeng/common/domain/user/controller/UserController.java
+++ backend/common/src/main/java/com/suhwakhaeng/common/domain/user/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.suhwakhaeng.common.domain.user.controller;
 
 import com.suhwakhaeng.common.domain.user.dto.ProfileResponse;
+import com.suhwakhaeng.common.domain.user.dto.UserInfoResponse;
 import com.suhwakhaeng.common.domain.user.service.UserService;
 import com.suhwakhaeng.common.global.common.dto.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -21,4 +19,11 @@ public class UserController {
         ProfileResponse profileResponse = userService.selectDetailUser(userId);
         return ResponseEntity.ok().body(Message.success(profileResponse));
     }
+
+    @GetMapping("/{userId}/info")
+    public ResponseEntity selectDetailUserInfo(@PathVariable Long userId) {
+        UserInfoResponse userInfoResponse = userService.selectDetailUserInfo(userId);
+        return ResponseEntity.ok().body(Message.success(userInfoResponse));
+    }
+
 }
--- backend/common/src/main/java/com/suhwakhaeng/common/domain/user/service/impl/UserServiceImpl.java
+++ backend/common/src/main/java/com/suhwakhaeng/common/domain/user/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.suhwakhaeng.common.domain.user.service.impl;
 
 import com.suhwakhaeng.common.domain.user.dto.ProfileResponse;
+import com.suhwakhaeng.common.domain.user.dto.UserInfoResponse;
 import com.suhwakhaeng.common.domain.user.entity.User;
 import com.suhwakhaeng.common.domain.user.repository.UserRepository;
 import com.suhwakhaeng.common.domain.user.service.UserService;
@@ -19,5 +20,12 @@ public class UserServiceImpl implements UserService {
     public ProfileResponse selectDetailUser(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException());
         return ProfileResponse.fromUser(user);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserInfoResponse selectDetailUserInfo(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException());
+        return UserInfoResponse.fromUser(user);
     }
 }
