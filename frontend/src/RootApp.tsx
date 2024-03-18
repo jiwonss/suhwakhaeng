f9f15e9ff683fbfa8e4f9fe41f2f265cfbc4567c
조은서 <eundeok9@gmail.com>
2024-03-18 15:22:17 +0900

    [FE] feat: SplashScreen, OauthScreen 설정 및 UI/UX 구현 완료

--- frontend/src/RootApp.tsx
+++ frontend/src/RootApp.tsx
@@ -1,11 +1,19 @@
-import { Text, View } from 'react-native';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { tokenState } from './recoil/atoms/tokenState';
 import AuthStack from './stacks/authStack/AuthStack';
 import MainStack from './stacks/mainStack/MainStack';
+import SplashScreen from 'react-native-splash-screen';
+import { useEffect } from 'react';
 
 export const RootApp = () => {
   const token = useRecoilValue(tokenState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('여기 언제와!');
+      SplashScreen.hide();
+    }, 500);
+  });
 
   if (token === '') {
     return <AuthStack />;
--- frontend/src/screens/auth/OauthScreen.tsx
+++ frontend/src/screens/auth/OauthScreen.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import * as Color from '../../config/color/Color';
+import * as Typo from '../../components/typography/Typography';
+import styled from 'styled-components/native';
+import { LocalImageLoader } from '../../components/image/ImageLoader';
+import { heightPercent } from '../../config/dimension/Dimension';
+import { login } from '@react-native-seoul/kakao-login';
+
+const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${Color.GREEN50};
+  position: 'relative';
+`;
+
+const LogoContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.TouchableOpacity`
+  position: absolute;
+  bottom: ${heightPercent * 70}px;
+`;
 
 const OauthScreen = () => {
+  const [result, setResult] = useState<string>('');
+
+  // TODO: 카카오 서버 accessToken 서버로 보내야함
+  const signInWithKakao = async () => {
+    try {
+      const token = await login();
+      setResult(JSON.stringify(token));
+    } catch (err) {
+      console.error('login err', err);
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Text>OauthScreen</Text>
-    </SafeAreaView>
+    <Container>
+      <LogoContainer>
+        <Typo.H1 color={Color.GREEN600}>수확행</Typo.H1>
+        <Typo.BODY2_M color={Color.GREEN400}>농작물 관리를 세상 쉽게, 수확행</Typo.BODY2_M>
+      </LogoContainer>
+      <ButtonContainer onPress={signInWithKakao}>
+        <LocalImageLoader source={require('../../../assets/imgs/kakaoButton.png')} />
+      </ButtonContainer>
+    </Container>
   );
 };
 
