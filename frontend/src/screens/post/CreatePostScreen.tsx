eca05a695adcca4c8c835f83fa7c637c14068218
오민상 <flos9537@gmail.com>
2024-03-19 11:36:54 +0900

    [FE] feat: 게시글 등록 페이지 사진 추가 및 하단 버튼 추가

--- frontend/src/screens/post/CreatePostScreen.tsx
+++ frontend/src/screens/post/CreatePostScreen.tsx
@@ -7,6 +7,9 @@ import { useState } from 'react';
 import { heightPercent, widthPercent } from '../../config/dimension/Dimension';
 import styled from 'styled-components/native';
 import { MultiLineInputBox } from '../../components/inputBox/Input';
+import ImgUploader from '../../components/imgUploader/ImgUploader';
+import { BasicButton } from '../../components/button/Buttons';
+import { Spacer } from '../../components/basic/Spacer';
 
 const Container = styled.View`
   margin-left: ${20 * widthPercent};
@@ -23,24 +26,33 @@ const CreatePostScreen = () => {
     { content: '나눔', event: () => setActiveIndex(2), active: activeIndex === 2 },
     { content: '질문', event: () => setActiveIndex(3), active: activeIndex === 3 },
   ];
-
+  const [Urls, setUrls] = useState<string[]>([]);
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: Color.WHITE }}>
-      <Header type={'default'} firstIcon='back' title={'게시글 등록'} onPressMore={() => console.log('더보기')} />
+    <View style={{ flex: 1, backgroundColor: Color.WHITE }}>
+      <ScrollView style={{ flex: 1, backgroundColor: Color.WHITE }}>
+        <Header type={'default'} firstIcon='back' title={'게시글 등록'} onPressMore={() => console.log('더보기')} />
+        <Spacer space={20} />
+        <Container>
+          <Typo.BODY4_M>분류 선택</Typo.BODY4_M>
+          <View style={{ alignItems: 'center' }}>
+            <CustomRadioButton data={Data} width={60} />
+          </View>
+        </Container>
+        <Container>
+          <Typo.BODY4_M>내용</Typo.BODY4_M>
+          <MultiLineInputBox placeholder={'내용을 작성하세요'} />
+        </Container>
+        <Container>
+          <Typo.BODY4_M>사진</Typo.BODY4_M>
+          <ImgUploader data={Urls} setData={setUrls}></ImgUploader>
+        </Container>
+      </ScrollView>
       <Container>
-        <Typo.BODY4_M>분류 선택</Typo.BODY4_M>
-        <View style={{ alignItems: 'center' }}>
-          <CustomRadioButton data={Data} width={60} />
-        </View>
+        <BasicButton borderColor={Color.GREEN500} borderRadius={10} height={45} onPress={() => console.log('작성완료')} width={300}>
+          <Typo.BODY3_M color={Color.WHITE}>작성완료</Typo.BODY3_M>
+        </BasicButton>
       </Container>
-      <Container>
-        <Typo.BODY4_M>내용</Typo.BODY4_M>
-        <MultiLineInputBox placeholder={'내용을 작성하세요'} />
-      </Container>
-      <Container>
-        <Typo.BODY4_M>사진</Typo.BODY4_M>
-      </Container>
-    </ScrollView>
+    </View>
   );
 };
 
