50fab595acf962369d406e4604ce5b961f6b1671
오민상 <flos9537@gmail.com>
2024-03-19 11:59:43 +0900

    [FE] feat: 게시글 수정 페이지 추가 및 Input 컴포넌트 수정
    - Input 컴포넌트에 children 속성 추가
    - 게시글 수정 페이지 작성

--- frontend/src/stacks/mainStack/MainStack.tsx
+++ frontend/src/stacks/mainStack/MainStack.tsx
@@ -3,17 +3,24 @@ import MainScreen from '../../screens/main/MainScreen';
 import FarmDairyAddScreen from '../../screens/farmDairy/FarmDairyAddScreen';
 import FarmDairyScreen from '../../screens/farmDairy/FarmDairyScreen';
 import FarmLedgerAddScreen from '../../screens/farmDairy/FarmLedgerAddScreen';
+import CreatePostScreen from '../../screens/post/CreatePostScreen';
+import DetailPostScreen from '../../screens/post/DetailPostScreen';
+import UpdatePostScreen from '../../screens/post/UpdatePostScreen';
 
 const Stack = createNativeStackNavigator();
 
 const MainStack = () => {
   return (
-    <Stack.Navigator initialRouteName='FarmDairyScreen'>
+    <Stack.Navigator initialRouteName='UpdatePostScreen'>
       <Stack.Screen name='MainScreen' component={MainScreen} options={{ headerShown: false }} />
       {/* 영농일지,장부 페이지 */}
       <Stack.Screen name='FarmDairyScreen' component={FarmDairyScreen} options={{ headerShown: false }} />
       <Stack.Screen name='FarmDairyAddScreen' component={FarmDairyAddScreen} options={{ headerShown: false }} />
       <Stack.Screen name='FarmLedgerAddScreen' component={FarmLedgerAddScreen} options={{ headerShown: false }} />
+      {/*게시글 CRU 페이지*/}
+      <Stack.Screen name='CreatePostScreen' component={CreatePostScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='DetailPostScreen' component={DetailPostScreen} options={{ headerShown: false }} />
+      <Stack.Screen name='UpdatePostScreen' component={UpdatePostScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
