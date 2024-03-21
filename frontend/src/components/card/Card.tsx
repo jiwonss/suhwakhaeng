2a8805ad47bdd1ac3584d077331bccd5c27c7509
오민상 <flos9537@gmail.com>
2024-03-21 10:29:36 +0900

    [FE] refactor: 카드컨테이너 스타일 수정
    min-height, padding-bottom, padding-top 추가

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 import * as Color from '../../config/color/Color';
-import { widthPercent } from '../../config/dimension/Dimension';
+import { heightPercent, widthPercent } from '../../config/dimension/Dimension';
 
 interface CardProps {
   backgroundColor?: string;
@@ -13,10 +13,13 @@ interface CardProps {
 const CardContainer = styled.View<CardProps>`
   width: ${(props) => (props.width ? props.width : 'auto')}px;
   height: ${(props) => (props.height ? props.height : 'auto')}px;
+  min-height: ${heightPercent * 30}px;
   border: ${(props) => (props.borderColor ? `1px solid ${props.borderColor}` : 'none')};
   border-radius: 10px;
   padding-left: ${widthPercent * 12}px;
   padding-right: ${widthPercent * 12}px;
+  padding-bottom: ${heightPercent * 5}px;
+  padding-top: ${heightPercent * 5}px;
   display: flex;
   justify-content: center;
   background-color: ${(props) => (props.backgroundColor ? props.backgroundColor : `${Color.WHITE}`)};
