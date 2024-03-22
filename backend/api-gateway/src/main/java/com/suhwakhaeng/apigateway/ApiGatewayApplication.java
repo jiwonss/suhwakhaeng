c2c6b92eb1d0038a53e2003d689ac997794acdd2
박정인 <wjddlsee@gmail.com>
2024-03-22 14:16:20 +0900

    [BE] feat: token 인증 로직 구현

--- backend/api-gateway/src/main/java/com/suhwakhaeng/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/suhwakhaeng/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.suhwakhaeng.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ApiGatewayApplication {
 
