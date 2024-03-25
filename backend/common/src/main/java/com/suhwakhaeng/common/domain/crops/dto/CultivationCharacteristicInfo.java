2c99c6959972ff5d2466bcef6244bffad851150e
서지원 <sjs3326@naver.com>
2024-03-25 14:48:40 +0900

    [BE] feat: 작물 상세조회 완료

--- backend/common/src/main/java/com/suhwakhaeng/common/domain/crops/dto/CultivationCharacteristicInfo.java
+++ backend/common/src/main/java/com/suhwakhaeng/common/domain/crops/dto/CultivationCharacteristicInfo.java
@@ -1,27 +1,19 @@
 package com.suhwakhaeng.common.domain.crops.dto;
 
-import com.suhwakhaeng.common.domain.crops.entity.CultivationCharacteristic;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
+@Setter
 @Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CultivationCharacteristicInfo {
 
+    private Long id;
     private String scientificName;
     private String classification;
     private String physiologicalCharacteristic;
     private String mainTech;
-
-    public static CultivationCharacteristicInfo fromCultivationCharacteristic(CultivationCharacteristic cultivationCharacteristic) {
-        return CultivationCharacteristicInfo.builder()
-                .scientificName(cultivationCharacteristic.getScientificName())
-                .classification(cultivationCharacteristic.getClassification())
-                .physiologicalCharacteristic(cultivationCharacteristic.getPhysiologicalCharacteristic())
-                .mainTech(cultivationCharacteristic.getMainTech())
-                .build();
-    }
 
 }
