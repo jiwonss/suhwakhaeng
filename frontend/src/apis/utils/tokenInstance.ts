f2fe9b0d9e4277f3ce3905bd791b11b3bba548a7
조은서 <eundeok9@gmail.com>
2024-03-25 11:56:48 +0900

    [FE] feat: tokenInstance 401 에러시 토큰 재발급, 재요청 기능 구현

--- frontend/src/apis/utils/tokenInstance.ts
+++ frontend/src/apis/utils/tokenInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import EncryptedStorage from 'react-native-encrypted-storage';
-import { getTokens, setTokens } from '../../util/TokenUtil';
+import { getTokens, removeTokens, setTokens } from '../../util/TokenUtil';
 
 const BASE_URL = 'http://10.0.2.2:8000/';
 
@@ -9,7 +9,6 @@ export const tokenInstance = axios.create({
 });
 
 const setCommonHeaders = async (config: any) => {
-  // console.log(`Bearer ${await EncryptedStorage.getItem('accessToken')}`);
   config.headers['Content-Type'] = 'application/json';
   config.headers['Authorization'] = `Bearer ${await EncryptedStorage.getItem('accessToken')}`;
   return config;
@@ -20,39 +19,46 @@ const reIssueAccessTokenAndRetry = async (config: AxiosRequestConfig) => {
   try {
     const response = await axios.post(`${BASE_URL}/common/reissue`, tokenData, { headers: { 'Content-Type': 'application/json' } });
     if (response.status === 201) {
-      // const newAccessToken = response.data.dataBody.accessToken;
-      // const newRefreshToken = response.datsa.dataBody.refreshToken;
-
-      setTokens(response.data.dataBody);
+      setTokens(response.data.dataBody); // 토큰 재 세팅
+      axios(config); // 재요청
     }
-  } catch {}
+    console.error('reIssueAccessTokenAndRetry error: ', response);
+    return Promise.reject(response);
+  } catch (error: any) {
+    console.error(error.response.data);
+    if (error.response.status === 401) {
+      alert('토큰 갱신에 실패했습니다. 다시 로그인 해주세요.');
+      removeTokens();
+      return Promise.reject(error);
+    }
+  }
 };
 
-// const handleResponseError = async (error: AxiosError) => {
-//   if (!error.response) return Promise.reject(error);
-//   const { status, data } = error.response as { status: number; data: any };
-//   console.log('status :', status, data);
+const handleResponseError = async (error: AxiosError) => {
+  if (!error.response) return Promise.reject(error);
+  const { status, config } = error.response;
+  console.log('status :', status);
 
-//   switch (status) {
-//     case 400:
-//       // alert('이미 매칭에 참여 중입니다');
-//       break;
-//     case 401:
-//     // TODO
-//     // 로그아웃 로직타기
-//     case 500:
-//       alert('시스템 에러, 관리자에게 문의 바랍니다.');
-//       break;
-//     default:
-//       console.error(error);
-//       return Promise.reject(error);
-//   }
-// };
+  switch (status) {
+    case 400:
+      // alert('이미 매칭에 참여 중입니다');
+      break;
+    case 401:
+      return await reIssueAccessTokenAndRetry(config);
 
-// const handleResponseSuccess = (response: AxiosResponse<any>) => {
-//   console.log('Success response: ' + response.config.url);
-//   return response;
-// };
+    case 500:
+      alert('시스템 에러, 관리자에게 문의 바랍니다.');
+      break;
+    default:
+      console.error(error);
+      return Promise.reject(error);
+  }
+};
+
+const handleResponseSuccess = (response: AxiosResponse<any>) => {
+  console.log('Success response: ' + response.config.url);
+  return response;
+};
 
 const handleRequestError = (error: AxiosError) => {
   console.error('handleRequestError :', error);
@@ -60,6 +66,6 @@ const handleRequestError = (error: AxiosError) => {
 };
 
 tokenInstance.interceptors.request.use(setCommonHeaders, handleRequestError);
-// tokenInstance.interceptors.response.use(handleResponseSuccess, handleResponseError);
+tokenInstance.interceptors.response.use(handleResponseSuccess, handleResponseError);
 
 export default tokenInstance;
