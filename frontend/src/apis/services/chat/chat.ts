b693f7b9a1405b1cbe67b48503b2f3ff611f0071
박정인 <wjddlsee@gmail.com>
2024-03-31 10:28:39 +0900

    [BE] fix: git문제 해결

--- frontend/src/apis/services/chat/chat.ts
+++ frontend/src/apis/services/chat/chat.ts
@@ -9,8 +9,3 @@ export const getChatList = async (chatRoomId: string) => {
   const response = await tokenInstance.get(`/chat/list/message/${chatRoomId}`);
   return response.data;
 };
-
-export const getChatLogList = async () => {
-  const response = await tokenInstance.get(`/chat/list/log`);
-  return response.data;
-};
