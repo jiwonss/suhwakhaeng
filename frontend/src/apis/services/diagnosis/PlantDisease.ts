f7f4fd86a2bcaada46c996c2c7714ca8c23d9dbe
오민상 <flos9537@gmail.com>
2024-04-01 03:17:10 +0900

    [FE] feat : 작물 진단 관련 기능 추가 및 수정
    진단전용 인스턴스 생성
    카메라 라이브러리 교체
    촬영시 스피너 보이도록 코드 수정
    작물 진단 초기 페이지 이름 보이도록 수정

--- frontend/src/apis/services/diagnosis/PlantDisease.ts
+++ frontend/src/apis/services/diagnosis/PlantDisease.ts
@@ -1,9 +1,23 @@
-import instance from '../../utils/basicInstance';
+import aiInstance from '../../utils/aiInstance';
+import { NavigationProp } from '@react-navigation/native';
+import { RootStackParamList } from '../../../stacks/mainStack/MainStack';
 
 // 사진 URL을 서버로 전송하는 함수
-export const sendPlantDiseaseImage = async (imageUrl: string) => {
-  const response = await instance.post('crop/disease', {
-    image: imageUrl, // image 필드에 직접 URL 문자열을 전송
-  });
-  return response.data;
+export const sendPlantDiseaseImage = async (imageUrl: string, navigation: NavigationProp<RootStackParamList>) => {
+
+  try {
+    console.log('이미지 주소:', imageUrl);
+    const response = await aiInstance.post('crop/disease', {
+      image: imageUrl,
+    });
+    if (response?.data) {
+      console.log('반환값:', response.data);
+      navigation.navigate('DetailDiseasePlantScreen', { photo: { uri: imageUrl }, diagnosisResult: response.data });
+
+    } else {
+      console.error('응답 데이터가 없습니다.');
+    }
+  } catch (error) {
+    console.error('API 호출 중 에러 발생:', error);
+  }
 };
---
+++ frontend/src/apis/utils/aiInstance.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+const BASE_URL = 'http://j10c103.p.ssafy.io:9002/';
+
+export const aiInstance = axios.create({
+  baseURL: `${BASE_URL}`,
+});
+
+const setCommonHeaders = async (config: any) => {
+  config.headers['Content-Type'] = 'application/json';
+  return config;
+};
+
+const handleResponseError = async (error: AxiosError) => {
+  if (!error.response) return Promise.reject(error);
+  const { status, data } = error.response as { status: number; data: any };
+  console.log('status :', status, data);
+
+  switch (status) {
+    case 400:
+      break;
+    case 401:
+    // TODO
+    // 로그아웃 로직타기
+    case 500:
+      alert('시스템 에러, 관리자에게 문의 바랍니다.');
+      break;
+    default:
+      console.error(error);
+      return Promise.reject(error);
+  }
+};
+
+const handleResponseSuccess = (response: AxiosResponse<any>) => {
+  console.log('Success response: ' + response.config.url);
+  return response;
+};
+
+const handleRequestError = (error: AxiosError) => {
+  console.error('handleRequestError :', error);
+  return Promise.reject(error);
+};
+
+aiInstance.interceptors.request.use(setCommonHeaders, handleRequestError);
+aiInstance.interceptors.response.use(handleResponseSuccess, handleResponseError);
+
+export default aiInstance;
