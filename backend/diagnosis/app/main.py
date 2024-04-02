7090cc8a074fbdb5d312fd96cd8988dee3c1ffe0
김수린 <uhyeon1009@gmail.com>
2024-04-03 07:16:44 +0900

    [AI] hotfix: input 데이터 파라미터 수정

--- backend/diagnosis/app/main.py
+++ backend/diagnosis/app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 
 class Image(BaseModel):
-    image: list
+    image: str
 
 
 app = FastAPI()
--- backend/diagnosis/app/model_files/ml_predict.py
+++ backend/diagnosis/app/model_files/ml_predict.py
@@ -20,58 +20,31 @@ def get_data(plant_disease):
 def predict_plant(imgdata):
     with open('model_files/labels2.json', 'rb') as lb:
         labels = pickle.load(lb)
-    print(labels)
+
     # Load the Keras model
     loaded_model = tf.keras.models.load_model("model_files/model2.h5")
-
-    result = []
-    # Converting Base64 string to Image
-    for i in imgdata:
-        response = requests.get(i)
-        new_image = Image.open(io.BytesIO(response.content))
-        new_image = new_image.resize((256, 256))
-
-        # Convert image to numpy array
-        # image_array = np.array(image)
-        image_array = image.img_to_array(new_image)
-
-        # # Reshape image array to match model input shape
-        image_array = np.expand_dims(image_array, axis=0)
-
-        # Getting prediction from model
-        y_result = loaded_model.predict(image_array)
-        result_idx = np.argmax(y_result)
-        result_idx = result_idx
-
-        # Getting Plant disease from result
-        for key, value in labels.items():
-            if value == result_idx:
-                plant_disease = key
-                break
-        result.append(get_data(plant_disease))
     
+    response = requests.get(imgdata)
+    new_image = Image.open(io.BytesIO(response.content))
+    new_image = new_image.resize((256, 256))
+
+    # Convert image to numpy array
+    # image_array = np.array(image)
+    image_array = image.img_to_array(new_image)
+
+    # # Reshape image array to match model input shape
+    image_array = np.expand_dims(image_array, axis=0)
+
+    # Getting prediction from model
+    y_result = loaded_model.predict(image_array)
+    result_idx = np.argmax(y_result)
+    result_idx = result_idx
     
-    # response = requests.get(imgdata)
-    # new_image = Image.open(io.BytesIO(response.content))
-    # new_image = new_image.resize((256, 256))
-
-    # # Convert image to numpy array
-    # # image_array = np.array(image)
-    # image_array = image.img_to_array(new_image)
-
-    # # # Reshape image array to match model input shape
-    # image_array = np.expand_dims(image_array, axis=0)
-
-    # # Getting prediction from model
-    # y_result = loaded_model.predict(image_array)
-    # result_idx = np.argmax(y_result)
-    # result_idx = result_idx
-    
-    # # Getting Plant disease from result
-    # for key, value in labels.items():
-    #     if value == result_idx:
-    #         plant_disease = key
-    #         break
-    # result = get_data(plant_disease)
+    # Getting Plant disease from result
+    for key, value in labels.items():
+        if value == result_idx:
+            plant_disease = key
+            break
+    result = get_data(plant_disease)
 
     return result
